5212a9ba003f97efe07ab74727aa627a594b74c5
radie <Radievskiy2003@gmail.com>
2023-09-06 22:29:13 +0300

    First hello world

--- src/main/java/com/example/spring_kpi_sem_5/SpringKpiSem5Application.java
+++ src/main/java/com/example/spring_kpi_sem_5/SpringKpiSem5Application.java
@@ -2,6 +2,9 @@ package com.example.spring_kpi_sem_5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class SpringKpiSem5Application {
@@ -10,4 +13,12 @@ public class SpringKpiSem5Application {
         SpringApplication.run(SpringKpiSem5Application.class, args);
     }
 
+    @RestController
+    public class HelloWorldController {
+        @GetMapping("/hello")
+        public String helloWorld() {
+            return "Hello, World!";
+        }
+    }
 }
+
