f753a46223e1e0a82e6831ac657ffebdd3af31b2
radie <Radievskiy2003@gmail.com>
2023-09-06 22:11:20 +0300

    Start

---
+++ src/main/java/com/example/spring_kpi_sem_5/SpringKpiSem5Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_kpi_sem_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKpiSem5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKpiSem5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_kpi_sem_5/SpringKpiSem5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_kpi_sem_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKpiSem5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
