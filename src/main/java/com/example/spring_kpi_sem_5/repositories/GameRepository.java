ce07d87fdb9e3029e32fd327ad8b0257ec98c834
radie <Radievskiy2003@gmail.com>
2023-10-19 17:02:22 +0300

    test2

--- src/main/java/com/example/spring_kpi_sem_5/repositories/GameRepository.java
+++ src/main/java/com/example/spring_kpi_sem_5/repositories/GameRepository.java
@@ -1,9 +1,12 @@
 package com.example.spring_kpi_sem_5.repositories;
 
 import com.example.spring_kpi_sem_5.entities.Game;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Date;
 
+@Repository
 public interface GameRepository {
     List<Game> findAll();
     List<Game> findByDate(Date date);
--- src/main/java/com/example/spring_kpi_sem_5/repositories/TeamRepository.java
+++ src/main/java/com/example/spring_kpi_sem_5/repositories/TeamRepository.java
@@ -1,8 +1,11 @@
 package com.example.spring_kpi_sem_5.repositories;
 
 import com.example.spring_kpi_sem_5.entities.Team;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface TeamRepository {
     List<Team> findAll();
     Team findByName(String name);
